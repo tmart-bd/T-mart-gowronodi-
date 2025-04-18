9bf6d7ce799faad887ba9a3a8f5d8eaf53d2926a
Tmart <itohid12@gmail.com>
2025-04-18 16:29:58 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
 // firebase-config.js
 
-// Firebase CDN দিয়ে ভার্সন 9 ব্যবহার করছি
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-storage.js";
 
-// তোমার Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyC4_YoA01AN3jEwbQAXyw6Q7GFty1g9BHg",
   authDomain: "t-mart-gowronodi.firebaseapp.com",
@@ -16,10 +14,6 @@ const firebaseConfig = {
   measurementId: "G-10M2C230N5"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-// এখন এই db আর storage ভেরিয়েবল অন্য ফাইলে ব্যবহার করা যাবে
-export { db, storage };
+export const db = getFirestore(app);
+export const storage = getStorage(app);
